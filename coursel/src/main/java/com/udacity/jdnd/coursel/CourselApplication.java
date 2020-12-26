cc561e97d95dff8223eac4b28181e70e2a8596cf
pavelshermayster <shermayster@gmail.com>
2020-12-26 12:30:46 +0200

    init

---
+++ coursel/src/main/java/com/udacity/jdnd/coursel/CourselApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.jdnd.coursel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourselApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourselApplication.class, args);
+	}
+
+}
---
+++ coursel/src/test/java/com/udacity/jdnd/coursel/CourselApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.jdnd.coursel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourselApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
